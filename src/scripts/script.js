cce7c0b9562763fe34faae57963a2685ac32a333
Talyslan <talyslancpc@gmail.com>
2023-06-19 21:15:34 -0300

    colocando a lógica

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,9 +1,38 @@
-let chooseCharacters = document.querySelectorAll(".back-character");
+let chooseChar = document.querySelectorAll(".back-character");
+let gifChar = document.querySelectorAll(".back-gif");
 
-let onMouseOver = (event) => {
-    console.log(event.target);
-} 
+function removeAllEffects() {
+    for (const div of chooseChar) {
+        div.classList.remove("select");
+    }
 
-for (let i = 0; i < chooseCharacters.length; i++) {
-    chooseCharacters[i].addEventListener("mouseover", onMouseOver);
+    for (const gifName of gifChar) {
+        gifName.classList.add("disappear");
+    }
+}
+
+const clickActive = (event) => {
+    removeAllEffects();
+    const divOnEvent = event.target;
+
+    if (divOnEvent.classList.contains("back-character")) {
+        removeAllEffects();
+
+        for (const gifName of gifChar) {
+            let nameGifVerification = gifName.children[1].innerHTML;
+            let nameCharVerification = divOnEvent.children[1].innerHTML;
+    
+            console.log(gifName)
+            
+            if (nameGifVerification == nameCharVerification) {
+                gifName.classList.remove("disappear");
+            }
+        }
+
+        divOnEvent.classList.add("select");
+    }
+}
+
+for (const div of chooseChar) {
+    div.addEventListener('click', clickActive, true);
 }
